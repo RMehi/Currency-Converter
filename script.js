21294653c03f01ee6cad9d2ad515f59d85962b25
RMehi <64398233+RMehi@users.noreply.github.com>
2022-04-23 13:11:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -15,25 +15,71 @@ async function getExchangeRate(from, to, amount) {
     return data;
 }
 
-
-// getExchangeRate(from, to, amount).then(data => {
-//     console.log(data)
-//     input[1].value = data.rates[`${to}`];
-// })
-
-getExchangeRate(from,to,amount).then((data)=>{
+getExchangeRate(from, to, amount).then((data) => {
     input[1].value = data.rates[`${to}`];
+})
 
-    first.forEach(item=>{
-        item.addEventListener("click",(e)=>{
-            from = e.target.innerText;
-            console.log(from)
+
+function changeColor(from, to) {
+    currencyList.forEach((item, index) => {
+        item = item.children;
+        for (let i = 0; i < item.length; i++) {
+            if (index == 0) {
+                item[i].style.backgroundColor = null;
+                item[i].style.color = '#C6C6C6';
+                if (item[i].id == from) {
+                    item[i].style.backgroundColor = "#833AE0";
+                    item[i].style.color = "#FFFFFF";
+                }
+            } else {
+                item[i].style.backgroundColor = null;
+                item[i].style.color = '#C6C6C6';
+                if (item[i].id == to) {
+                    item[i].style.backgroundColor = "#833AE0";
+                    item[i].style.color = "#FFFFFF";
+                }
+            }
+        }
+    })
+}
+
+function getInput() {
+    input.forEach(item => {
+        item.addEventListener("keyup", (e) => {
+            amount = item.value;
+            return amount;
         })
     })
-    
-    second.forEach(item=>{
-        item.addEventListener("click",(e)=>{
-            to = e.target.innerText;
-            console.log(to)
+}
+
+first.forEach(item => {
+    item.addEventListener("click", (e) => {
+        from = e.target.innerText;
+
+        changeColor(from, to);
+
+        getInput(amount);
+
+        getExchangeRate(from, to, amount).then((data) => {
+            input[1].value = data.rates[`${to}`];
         })
-    })})
+
+    })
+})
+
+second.forEach(item => {
+    item.addEventListener("click", (e) => {
+        to = e.target.innerText;
+
+        changeColor(from, to);
+
+        getInput(amount);
+
+        getExchangeRate(from, to, amount).then((data) => {
+            input[1].value = data.rates[`${to}`];
+        })
+
+    })
+})
+
+changeColor(from, to);
